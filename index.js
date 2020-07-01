707eabe7b94d38ca5266d8dbcebc88bcd0dba5ec
gianlucaparadise <gianlucaparadise@gmail.com>
2020-07-01 08:57:12 +0200

    Release 1.5.8 - added some comments - added Github npm registry

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const CalendarHelper = require('./calendar-helper');
 
 class Calendar {
+	/**
+	 * Construct the calendar
+	 * @param {Telegraf} bot Telegraf bot instance
+	 * @param {*} options Options to configure the calendar
+	 */
 	constructor(bot, options) {
 		this.bot = bot
 
@@ -9,6 +14,7 @@ class Calendar {
 
 	/**
 	 * Return Calendar Markup
+	 * @param {Date} date Starting date for the calendar. When null, 'today' is used
 	 */
 	getCalendar(date) {
 		if (!date) date = new Date();
@@ -16,6 +22,10 @@ class Calendar {
 		return this.helper.getCalendarMarkup(date);
 	}
 
+	/**
+	 * Set the callback that will be called when a date is selected
+	 * @param {(context: Context, date: Date) => void} onDateSelected The callback to be used
+	 */
 	setDateListener(onDateSelected) {
 		this.bot.action(/calendar-telegram-date-[\d-]+/g, context => {
 			if (onDateSelected) {
@@ -48,26 +58,46 @@ class Calendar {
 		this.bot.action(/calendar-telegram-ignore-[\d\w-]+/g, context => context.answerCbQuery());
 	}
 
+	/**
+	 * Minimum selectable date
+	 * @param {Date} date The date to be used
+	 */
 	setMinDate(date) {
 		this.helper.setMinDate(new Date(date));
 		return this;
 	}
 
+	/**
+	 * Maximum selectable date
+	 * @param {Date} date The date to be used
+	 */
 	setMaxDate(date) {
 		this.helper.setMaxDate(new Date(date));
 		return this;
 	}
 
+	/**
+	 * Set the week day names, where the first element is `startWeekDay` name
+	 * @param {String[]} names Names to be used
+	 */
 	setWeekDayNames(names) {
 		this.helper.setWeekDayNames(names);
 		return this;
 	}
 
+	/**
+	 * Set the month names
+	 * @param {String[]} names Names to be used
+	 */
 	setMonthNames(names) {
 		this.helper.setMonthNames(names);
 		return this;
 	}
 
+	/**
+	 * Set the first day of the week, where 0 is Sunday
+	 * @param {Number} startDay Day to be used
+	 */
 	setStartWeekDay(startDay) {
 		this.helper.setStartWeekDay(startDay);
 		return this;
